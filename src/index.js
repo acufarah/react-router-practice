3e86cf0dbd746bb60d52200b16e231aed2f6705c
Farah Khan <farahkhanu@gmail.com>
2018-08-29 23:14:57 +0000

    added links to header

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 //Components
 import Home from './components/home';
@@ -11,9 +11,15 @@ const App = () =>{
     return (
         <BrowserRouter>
             <div>
-                <Route path ="/posts" component = {Posts}/>
-                <Route path ="/profile" component = {Profile}/>
-                <Route path ="/" component = {Home}/>
+                <header>
+                    <Link to='/'>Home</Link><br/>
+                    <Link to='/posts'>Posts</Link><br/>
+                    <Link to='/profile'>Profile</Link><br/>
+                    <hr/>
+                </header>
+                <Route path ="/posts" exact component = {Posts}/>
+                <Route path ="/profile" exact component = {Profile}/>
+                <Route path ="/" exact component = {Home}/>
             </div>
         </BrowserRouter>
         );
